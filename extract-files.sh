c6f6fe988b645f92c55023ca812add2c93363fba
Luofan Chen <clfbbn@gmail.com>
2024-10-08 15:36:20 +0000

    pdx234: Setup ELF Check
    Change-Id: Ie4f610a2f072b2fc15f908c70222e013b044476f

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,29 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/vendor.semc.hardware.extlight-V1-ndk_platform.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "android.hardware.light-V1-ndk_platform.so" "android.hardware.light-V1-ndk.so" "${2}"
+            ;;
+        vendor/lib64/libarcsoft_hdr_adapter.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "liblog.so" "${2}"
+            "${PATCHELF}" --add-needed "libcutils.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
