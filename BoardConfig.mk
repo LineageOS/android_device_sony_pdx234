0f278802ed719e55e42d946ca57551d24f721396
Luofan Chen <clfbbn@gmail.com>
2024-10-10 07:50:52 +0000

    pdx234: Config OTA assert device
    Change-Id: I3c9c22a8b943eecde69cc97da3b98b949615097b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,5 +25,8 @@ TARGET_SCREEN_DENSITY := 420
 # Props
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# OTA
+TARGET_OTA_ASSERT_DEVICE := pdx234,XQ-DQ72
+
 # inherit from the proprietary version
 -include vendor/sony/pdx234/BoardConfigVendor.mk
