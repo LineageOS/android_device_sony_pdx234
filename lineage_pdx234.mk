a6799f664e614ce8e35ba15229858062119abed0
Luofan Chen <clfbbn@gmail.com>
2025-01-13 14:35:19 +0000

    pdx234: Update blobs to XQ-DQ62_Customized_US_67.2.A.2.41
    Change-Id: Ib34bea195e597c307f8197c806850b74521c6c40

--- lineage_pdx234.mk
+++ lineage_pdx234.mk
@@ -31,8 +31,8 @@ PRODUCT_BUILD_PROP_OVERRIDES := \
     DeviceProduct=XQ-DQ72 \
     SystemDevice=XQ-DQ72 \
     SystemName=XQ-DQ72 \
-    BuildFingerprint=Sony/XQ-DQ72/XQ-DQ72:14/67.1.A.2.307/067001A002030700521143226:user/release-keys \
-    BuildDesc="XQ-DQ72-user 14 67.1.A.2.307 067001A002030700521143226 release-keys"
+    BuildFingerprint=Sony/XQ-DQ62/XQ-DQ62:15/67.2.A.2.41/067002A002004100521143226:user/release-keys \
+    BuildDesc="XQ-DQ62-user 15 67.2.A.2.41 067002A002004100521143226 release-keys"
 
 PRODUCT_NAME := lineage_pdx234
 PRODUCT_DEVICE := pdx234
